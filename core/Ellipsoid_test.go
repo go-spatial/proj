32bba3d5f96e45d22ef4255622bd6dac1b21c249
mpg <mpg@flaxen.com>
2018-04-19 21:24:04 -0400

    code coverage

--- core/Ellipsoid_test.go
+++ core/Ellipsoid_test.go
@@ -1,12 +1,27 @@
 package core_test
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/go-spatial/proj/core"
+	"github.com/go-spatial/proj/support"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEllipsoid(t *testing.T) {
 	assert := assert.New(t)
-	assert.True(true)
+
+	ps, err := support.NewProjString("+proj=utm +zone=32 +ellps=GRS80")
+	assert.NoError(err)
+	sys, _, err := core.NewSystem(ps)
+	assert.NoError(err)
+
+	e := sys.Ellipsoid
+
+	assert.Equal("GRS80", e.ID)
+
+	s := fmt.Sprintf("%s", e)
+	assert.True(len(s) > 1)
 }
