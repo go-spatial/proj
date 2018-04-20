bbe6fb154044151de02e6c23adc990ee4ab88a10
mpg <mpg@flaxen.com>
2018-04-19 20:56:12 -0400

    update tests

--- merror/Error_test.go
+++ merror/Error_test.go
@@ -8,22 +8,26 @@ import (
 )
 
 func TestError(t *testing.T) {
+	showSource := merror.ShowSource
+	merror.ShowSource = true
+	defer func() { merror.ShowSource = showSource }()
+
 	assert := assert.New(t)
 
 	err1 := merror.New("errtest-%d", 1)
 	assert.Error(err1)
-	exp1 := "errtest-1 (from merror_test.TestError at Error_test.go:13)"
+	exp1 := "errtest-1 (from merror_test.TestError at Error_test.go:17)"
 	assert.Equal(exp1, err1.Error())
 
 	err2 := merror.Wrap(err1, "errtest-%d", 2)
 	assert.Error(err2)
-	exp2 := "errtest-2 (from merror_test.TestError at Error_test.go:18)"
+	exp2 := "errtest-2 (from merror_test.TestError at Error_test.go:22)"
 	exp2 += " // Inner: " + exp1
 	assert.Equal(exp2, err2.Error())
 
 	err3 := merror.Wrap(err2)
 	assert.Error(err3)
-	exp3 := "wrapped error (from merror_test.TestError at Error_test.go:24)"
+	exp3 := "wrapped error (from merror_test.TestError at Error_test.go:28)"
 	exp3 += " // Inner: " + exp2
 	assert.Equal(exp3, err3.Error())
 }
