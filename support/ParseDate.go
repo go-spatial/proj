df3347579066985b8da3872740c2a3845971cffd
mpg <mpg@flaxen.com>
2018-04-20 14:49:16 -0400

    added copyright header to all files (#25)

--- support/ParseDate.go
+++ support/ParseDate.go
@@ -1,3 +1,10 @@
+// Copyright (C) 2018, Michael P. Gerlek (Flaxen Consulting)
+//
+// Portions of this code were derived from the PROJ.4 software
+// In keeping with the terms of the PROJ.4 project, this software
+// is provided under the MIT-style license in `LICENSE.md` and may
+// additionally be subject to the copyrights of the PROJ.4 authors.
+
 package support
 
 import "strconv"
