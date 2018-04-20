cc4372577a43846578df741f4b2480971bb67f11
mpg <mpg@flaxen.com>
2018-04-19 20:48:23 -0400

    implemented app & tests

--- cmd/proj/proj.go
+++ cmd/proj/proj.go
@@ -3,34 +3,148 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
+	"github.com/go-spatial/proj"
+
+	"github.com/go-spatial/proj/core"
+	"github.com/go-spatial/proj/merror"
+	"github.com/go-spatial/proj/mlog"
 	"github.com/go-spatial/proj/support"
 )
 
 func main() {
-
-	verbose := flag.Bool("verbose", false, "do lots of logging")
-	inverse := flag.Bool("inverse", false, "go backwards")
-
-	flag.Parse()
-
-	fmt.Printf("verbose: %t\n", *verbose)
-	fmt.Printf("inverse: %t\n", *inverse)
-
-	s := strings.Join(flag.Args(), " ")
-
-	s = "+proj=utm +zone=32 +ellps=GRS80" // TODO
-
-	fmt.Printf("string: %s\n", s)
-
-	_, err := support.NewProjString(s)
+	err := Main(os.Stdin, os.Stdout, os.Args)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		os.Exit(1)
+	}
+}
+
+// Main is just a callable version of main(), for testing purposes
+func Main(inS io.Reader, outS io.Writer, args []string) error {
+
+	merror.ShowSource = false
+	mlog.DebugEnabled = false
+	mlog.InfoEnabled = false
+	mlog.ErrorEnabled = false
+
+	cli := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	cli.SetOutput(outS)
+
+	verbose := cli.Bool("verbose", false, "enable logging")
+	inverse := cli.Bool("inverse", false, "run the inverse transform")
+	epsgDest := cli.Int("epsg", 0, "perform conversion from 4326 to given destination system")
+
+	err := cli.Parse(args[1:])
+	if err != nil {
+		return err
+	}
+	projString := strings.Join(cli.Args(), " ")
+
+	if *verbose {
+		mlog.Printf("verbose: %t", *verbose)
+		mlog.Printf("inverse: %t", *inverse)
+		if *epsgDest == 0 {
+			mlog.Printf("epsg: (not specified)")
+		} else {
+			mlog.Printf("epsg: %d", epsgDest)
+		}
+		if projString == "" {
+			mlog.Printf("proj: (not specified)")
+		} else {
+			mlog.Printf("proj: %s", projString)
+		}
+
+		merror.ShowSource = true
+		mlog.DebugEnabled = true
+		mlog.InfoEnabled = true
+		mlog.ErrorEnabled = true
 	}
 
-	var a1, a2 float64
-	fmt.Fscanf(os.Stdin, "%f %f\n", &a1, &a2)
-	fmt.Printf("-> %f %f\n", a1, a2)
+	if *epsgDest != 0 {
+		if *inverse {
+			return fmt.Errorf("-inverse not allowed with -epsg")
+		}
+		if projString != "" {
+			return fmt.Errorf("projection string not allowed with -epsg")
+		}
+		input := make([]float64, 2)
+
+		f := func(a, b float64) (float64, float64, error) {
+			input[0] = a
+			input[1] = b
+			output, err := proj.Convert(proj.EPSGCode(*epsgDest), input)
+			if err != nil {
+				return 0.0, 0.0, err
+			}
+			return output[0], output[1], nil
+		}
+
+		return repl(inS, outS, f)
+	}
+
+	ps, err := support.NewProjString(projString)
+	if err != nil {
+		return err
+	}
+
+	_, opx, err := core.NewSystem(ps)
+	if err != nil {
+		return err
+	}
+
+	op := opx.(core.IConvertLPToXY)
+
+	if !*inverse {
+
+		f := func(a, b float64) (float64, float64, error) {
+			input := &core.CoordLP{Lam: support.DDToR(a), Phi: support.DDToR(b)}
+			output, err := op.Forward(input)
+			if err != nil {
+				return 0.0, 0.0, err
+			}
+			return output.X, output.Y, nil
+		}
+		return repl(inS, outS, f)
+	}
+
+	f := func(a, b float64) (float64, float64, error) {
+		input := &core.CoordXY{X: a, Y: b}
+		output, err := op.Inverse(input)
+		if err != nil {
+			return 0.0, 0.0, err
+		}
+		return support.RToDD(output.Lam), support.RToDD(output.Phi), nil
+	}
+	return repl(inS, outS, f)
+}
+
+type converter func(a, b float64) (float64, float64, error)
+
+func repl(inS io.Reader, outS io.Writer, f converter) error {
+
+	var a, b float64
+
+	for {
+		n, err := fmt.Fscanf(inS, "%f %f\n", &a, &b)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if n != 2 {
+			return fmt.Errorf("error reading input")
+		}
+
+		c, d, err := f(a, b)
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(outS, "%f %f\n", c, d)
+	}
 }
--- cmd/proj/proj_test.go
+++ cmd/proj/proj_test.go
@@ -1,12 +1,70 @@
 package main_test
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
+	main "github.com/go-spatial/proj/cmd/proj"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestXYZZY(t *testing.T) {
+func TestCmd(t *testing.T) {
 	assert := assert.New(t)
-	assert.True(true)
+
+	type testcase struct {
+		args   string
+		input  []float64
+		output []float64
+	}
+
+	testcases := []testcase{
+		{
+			"proj -epsg 9999",
+			[]float64{0.0, 0.0},
+			nil,
+		}, {
+			"proj -epsg 9999 proj=merc",
+			[]float64{0.0, 0.0},
+			nil,
+		}, {
+			"proj-epsg 9999 -inverse",
+			[]float64{0.0, 0.0},
+			nil,
+		}, {
+			"proj -epsg 3395",
+			[]float64{-77.625583, 38.833846},
+			[]float64{-8641240.37, 4671101.60},
+		}, {
+			"proj +proj=utm +zone=32 +ellps=GRS80",
+			[]float64{12.0, 55.0},
+			[]float64{691875.63, 6098907.83},
+		},
+	}
+
+	for _, tc := range testcases {
+
+		s := fmt.Sprintf("%f %f", tc.input[0], tc.input[1])
+		inBuf := bytes.NewBuffer([]byte(s))
+		outBuf := &bytes.Buffer{}
+
+		err := main.Main(inBuf, outBuf, strings.Fields(tc.args))
+
+		if tc.output == nil {
+			assert.Error(err, tc.args)
+		} else {
+			assert.NoError(err, tc.args)
+
+			tokens := strings.Fields(string(outBuf.Bytes()))
+			assert.Len(tokens, 2, tc.args)
+			actual0, err := strconv.ParseFloat(tokens[0], 64)
+			assert.NoError(err, tc.args)
+			assert.InDelta(tc.output[0], actual0, 1.0e-2, tc.args)
+			actual1, err := strconv.ParseFloat(tokens[1], 64)
+			assert.NoError(err, tc.args)
+			assert.InDelta(tc.output[1], actual1, 1.0e-2, tc.args)
+		}
+	}
 }
